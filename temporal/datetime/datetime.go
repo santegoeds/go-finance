f7d87c1e03d1c953943910558c61ea822867f1f9
Tjerk Santegoeds <tsan@tdias.com>
2011-05-17 21:55:04 +0100

    Incremental check-in

---
+++ temporal/datetime/datetime.go
@@ -0,0 +1,49 @@
+package datetime
+
+import (
+    "time"
+    "../date/_obj/date"
+)
+
+type DateTime struct {
+    *date.Date
+    time int
+}
+
+func Now() *DateTime {
+    t := time.LocalTime()
+    return NewDateTime(int(t.Year), t.Month, t.Day, t.Hour, t.Minute, t.Second)
+}
+
+func NewDateTime(args ...int) *DateTime {
+    var hour, minute, second int
+
+    year, month, day := 1970, 1, 1
+
+    for i, v := range args {
+        switch i {
+        case 0:
+            year = v
+            break
+        case 1:
+            month = v
+            break
+        case 2:
+            day = v
+            break
+        case 3:
+            hour = v
+            break
+        case 4:
+            minute = v
+            break
+        case 5:
+            second = v
+            break
+        }
+    }
+    second += minute * 60 + hour * 3600
+    d := date.NewDate(year, month, day)
+    return &DateTime{d, second}
+}
+
